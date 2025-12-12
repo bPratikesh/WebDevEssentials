0a70ed7c115e5364284dd6d7cf80c994e82ded90
Pratikesh <pratikeshb9@gmail.com>
2025-12-12 22:27:37 +0530

    operators JS demo

---
+++ Week1JS/basic.js
@@ -0,0 +1,12 @@
+document.write("Hey");
+// alert("Profile updated");
+console.log("new log");
+var x = 10;
+var y = 20;
+const pi = 3.14;
+
+document.write(x);
+document.write("<br></br>");
+document.write(y);
+document.write("<br></br>");
+document.write(pi);
