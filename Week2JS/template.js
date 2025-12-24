48162c8fde6b96923bc4cc0dcb61fb9faa5c3f8c
Pratikesh <pratikeshb9@gmail.com>
2025-12-24 20:44:53 +0530

    demo JS program

---
+++ Week2JS/template.js
@@ -0,0 +1,17 @@
+var a = 5;
+var b = 3;
+console.log(`The sum of ${a} and ${b} is: ${a + b}`);
+
+var name = "Pratikesh";
+console.log(`Hi ${name},
+    Requesting for a leave.
+TY`);
+
+var student = {
+  id: 101,
+  hobbies: ["Singing", "Reading"],
+};
+
+console.log(
+  `Student Id is ${student.id} and hobbies are ${student.hobbies[0]}`
+);
