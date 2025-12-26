305e33f2f341032e3fc0033d32ac3057ec94a8c1
Pratikesh <pratikeshb9@gmail.com>
2025-12-26 20:10:32 +0530

    destruction

--- Week2JS/dest.js
+++ Week2JS/dest.js
@@ -12,3 +12,14 @@ console.log(id, Name);
 var even = [2, 4, 6, 8];
 var [a, b, c, d] = even;
 console.log(a, d);
+
+//Alliasing
+var employee = {
+  eno: 101,
+  ename: "Pratikeshhhhh",
+  ePhone: "8452019787",
+};
+
+var { eno, ename: emp_name, ePhone } = employee;
+
+console.log(emp_name);
